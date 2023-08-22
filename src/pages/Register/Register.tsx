11f3b2a8953cce339581e30515725cbabe42a9ec
David Dehe <david.dehe.pro@gmail.com>
2023-08-22 16:32:23 +0200

    :sparkles: / add pre writed input when user redirected to regitster page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,12 +1,13 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import { Header } from "../../components/Blocks/Header/Header";
+import { ParraingageCodeForm } from "../../components/Elements/ParraingageCodeForm/ParraingageCodeForm";
 import "./Register.scss";
 const Register: React.FC = () => {
   return (
     <IonPage id="main-content" className="container">
       <div className="headerContainer">
         <Header />
-        <p>register</p>
+        <ParraingageCodeForm />
       </div>
     </IonPage>
   );
